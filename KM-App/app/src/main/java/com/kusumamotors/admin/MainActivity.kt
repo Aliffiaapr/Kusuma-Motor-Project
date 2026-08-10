e301f2524d8724c7661d973f3052162f1d227bf9
Aliffiaapr <aliffiaanggun4@gmail.com>
2026-08-10 11:27:59 +0700

    setup halaman login, fragment untuk dashboard

--- KM-App/app/src/main/java/com/kusumamotors/admin/MainActivity.kt
+++ KM-App/app/src/main/java/com/kusumamotors/admin/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.kusumamotors.admin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // Halaman awal default saat aplikasi dibuka: Dashboard
+        loadFragment(DashboardFragment())
+
+        // Logika perpindahan halaman saat menu diklik
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> loadFragment(DashboardFragment())
+                R.id.nav_soon -> loadFragment(MendatangFragment())
+                R.id.nav_riwayat -> loadFragment(RiwayatFragment())
+                R.id.nav_setting -> loadFragment(SettingFragment())
+            }
+            true
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
---
+++ KM-App/app/src/main/java/com/kusumamotors/admin/MendatangFragment.kt
@@ -0,0 +1,16 @@
+package com.kusumamotors.admin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MendatangFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_mendatang, container, false)
+    }
+}
